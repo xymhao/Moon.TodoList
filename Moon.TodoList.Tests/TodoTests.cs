ba03c31d33ba775ce36c8c4b198d9eebceaa3481
yueming xu <xnysym@outlook.com>
2020-12-30 18:05:21 +0800

    add todo item

---
+++ Moon.TodoList.Tests/TodoTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace Moon.TodoList.Tests
+{
+    public class TodoTests
+    {
+        [Fact]
+        public void Should_Success_When_Add_Item_Abc()
+        {
+            var todo = new Todo(null);
+
+            todo.Add("abc");
+        }
+    }
+}
---
+++ Moon.TodoList/Todo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace Moon.TodoList
+{
+    public class Todo
+    {
+        private readonly string _filePath;
+
+        public Todo(string filePath)
+        {
+            _filePath = string.IsNullOrEmpty(filePath)
+                ? Environment.CurrentDirectory + @"\" + "MoonTodoList.txt"
+                : filePath;
+        }
+        
+        public void Add(string item)
+        {
+            File.WriteAllText(_filePath,item);
+        }
+    }
+}
